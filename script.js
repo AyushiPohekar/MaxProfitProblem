bcbc073be95eeb01f1d959bbac7922e72b9cde45
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-06-18 12:15:19 +0100

    done

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// let T_Time=5;
-// let P_Time=4;
-// let C_Time=10;
-let T_Money = 1500;
-let P_Money = 1000;
-let C_Money = 3000;
-let T_Earn;
-let P_Earn;
-let C_Earn;
-let T_Count = 0;
-let P_Count = 0;
-let C_Count = 0;
-let sum = 0;
+//Possibility of Multiple Outputs:There are 3 possible combinations for multiple outputs.
+//Theatre+Pub=7units //we will calculate
+//Pub+Commercial Park=13 units //Here Theatre gives maximum value
+//Theatre+Commercial=15 units 
 
-function MaxProfit(input1) {
-  console.log("MaxProfit", input1);
+function MaxProfit(input1, flag, T_Count, P_Count, C_Count, sum) {
+  let T_Earn;
+  let P_Earn;
+  let C_Earn;
+
+  //Checking for multiple output when theatre+pub earn maximum.
+  if (input1 == 7) {
+    flag = 1;
+  }
 
   if (input1 > 5) {
     T_Earn = (input1 - 5) * 1500;
@@ -33,8 +31,6 @@ function MaxProfit(input1) {
     C_Earn = 0;
   }
 
-  console.log(T_Earn, P_Earn, C_Earn);
-
   let max = Math.max(T_Earn, P_Earn, C_Earn);
   console.log(max);
   sum = sum + max;
@@ -46,33 +42,51 @@ function MaxProfit(input1) {
       input1 = input1 - 4;
       P_Count++;
     } else {
+      //since I wrote else,this is important for Pub+Commercial once T_Earn is maximum,it won't go in else block and C_count is not updated
       input1 = input1 - 10;
       C_Count++;
     }
   }
 
-  console.log(input1);
-  console.log(`T:${T_Count}`, `P:${P_Count}`, `C:${C_Count}`);
-  return input1;
+  //console.log(`T:${T_Count}`, `P:${P_Count}`, `C:${C_Count}`);
+  return [input1, flag, T_Count, P_Count, C_Count, sum];
 }
 
 function InputValue() {
   let input = document.getElementById("inputTime").value;
-  sum=0;
-  T_Count=0;
-  P_Count=0;
-  C_Count=0;
+  let sum = 0;
+  let T_Count = 0;
+  let P_Count = 0;
+  let C_Count = 0;
+  let flag = 0;
 
-  console.log("inputValue", input);
-  //MaxProfit(input)
   count_while = 0;
+
+  let i = 0;
   while (input > 4) {
-    input = MaxProfit(input);
+    [input, flag, T_Count, P_Count, C_Count, sum] = MaxProfit(
+      input,
+      flag,
+      T_Count,
+      P_Count,
+      C_Count,
+      sum
+    );
+    i++;
   }
   let earning = (document.getElementById("sum").innerHTML = `$${sum}`);
-  let solution = (document.getElementById(
-    "solution"
-  ).innerHTML = `T:${T_Count},P:${P_Count},C:${C_Count}`);
+  let solution = document.getElementById("solution");
+
+
+  //flag=1 indicates multiple outputs.
+  if (flag == 1) {
+    solution.innerHTML = `Multiple Possible Solution:
+     Solution1=>T:${T_Count},P:${P_Count},C:${C_Count}
+     Solution2=>T:${T_Count - 1},P:${P_Count + 1},C:${C_Count}`;
+  } else {
+    solution.innerHTML = `T:${T_Count},P:${P_Count},C:${C_Count}`;
+  }
+
   console.log(sum);
 }
 InputValue();
